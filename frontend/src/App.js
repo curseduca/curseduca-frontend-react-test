29bcf8f9514769a52942694e7464683f01914531
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-08 19:56:03 -0300

    Usando Feed container no App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Auth from './Container/Auth/Auth';
+// import Auth from './Container/Auth/Auth';
+import Feed from './Container/Feed/Feed';
 import Layout from './Container/Layout/Layout';
 
 function App() {
   return (
     <Layout>
       <div>
-        <Auth />
+        {/* <Auth /> */}
+        <Feed />
       </div>
     </Layout>
   );
