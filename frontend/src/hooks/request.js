f8eee615605ca54d6c6214b35c7305bb8e1922b5
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-08 18:29:29 -0300

    Criado hook para requests e tambem instancia de axios com a baseURL settada

---
+++ frontend/src/hooks/request.js
@@ -0,0 +1,73 @@
+import { useReducer, useEffect, useCallback } from 'react';
+
+const initialState = {
+  loading: false,
+  error: null,
+  data: null
+};
+
+const requestReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        ...state,
+        error: null,
+        data: null,
+        loading: action.status
+      };
+    case 'ERROR':
+      return {
+        ...state,
+        data: null,
+        loading: false,
+        error: action.error
+      };
+    case 'DATA':
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        data: action.data
+      };
+    case 'CLEAR':
+      return {
+        ...initialState
+      };
+    default:
+      throw new Error('[useRequest reducer] unknown action: ', action.type);
+  }
+};
+
+export default (options, axios) => {
+  // options: request options (method, headers, etc)
+  const [requestState, dispatch] = useReducer(requestReducer, initialState);
+
+  const fetchData = useCallback(async () => {
+    dispatch({ type: 'LOADING', status: true });
+    try {
+      const request = await axios({
+        ...options,
+        responseType: 'json'
+      });
+      dispatch({ type: 'DATA', data: request.data });
+    } catch (err) {
+      if (err.response) {
+        dispatch({ type: 'ERROR', error: err.response.data });
+      } else if (err.request) {
+        dispatch({ type: 'ERROR', error: 'Couldn\'t reach server.' });
+      } else {
+        dispatch({ type: 'ERROR', error: 'Couldn\'t reach server.' });
+      }
+    }
+  }, [options]);
+
+  const clearState = () => dispatch({ type: 'CLEAR' });
+
+  useEffect(() => {
+    if (options && options.method) {
+      fetchData();
+    }
+  }, [options, fetchData]);
+
+  return [requestState, clearState];
+};
