d21e0686f423ea944fa12b9228470ce4eee2b565
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-08 21:44:02 -0300

    Importando css do bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import userReducer from './store/userReducer';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
