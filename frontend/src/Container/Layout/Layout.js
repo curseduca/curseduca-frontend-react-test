9d96401f9e48b5c1270fcda26bf614e293892ce1
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-08 21:22:21 -0300

    Portado Layout Container para bootstrap

--- frontend/src/Container/Layout/Layout.js
+++ frontend/src/Container/Layout/Layout.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import { Container, Row, Col } from 'react-bootstrap';
 
-const Layout = ({ children }) => {
-  const obj = {
-    standard: {
-      color: '#FFF',
-      hover: '#bb86fc',
-      inverted: '#000',
-      lighter: '#1d1d1d',
-      darker: '#121212'
-    }
-  };
-
-  return (
-    <ThemeProvider theme={obj.standard}>
-      {children}
-    </ThemeProvider>
-  );
-};
+const Layout = ({ children }) => (
+  <Container>
+    <Row>
+      <Col>
+        {children}
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default Layout;
